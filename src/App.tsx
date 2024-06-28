0130ceef9b17d4d7dbdcee4786e48d51ed036bad
ugla <montagu666@gmail.com>
2024-06-28 15:39:29 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {response} from "./data/response.ts";
+import {useState} from "react";
+
+type Character = {}
+
+function App() {
+    const [response, setResponse] = useState<Response>();
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default App
